55c1a08e3e4a4d0645afce28a21406e7f86a0b75
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-08-25 00:14:12 +0330

    add scroll controller to explore listviews widget

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:nft_marketplace/dummy_data.dart';
-import 'package:nft_marketplace/gen/assets.gen.dart';
 import 'package:nft_marketplace/models/nft_model.dart';
 
 class ExploreScreen extends StatelessWidget {
@@ -13,19 +15,22 @@ class ExploreScreen extends StatelessWidget {
       body: Transform.rotate(
         angle: -0.08,
         child: OverflowBox(
-          maxWidth: size.width*1.5,
+          maxWidth: size.width * 1.5,
           child: Column(children: [
             ListWidget(
               items: DummyDate.exploreFirstList,
+              duration: 80,
             ),
             ListWidget(
               items: DummyDate.exploreSecondList,
             ),
             ListWidget(
               items: DummyDate.exploreItemThirdList,
+              duration: 120,
             ),
             ListWidget(
               items: DummyDate.exploreItemFourthList,
+              duration: 90,
             )
           ]),
         ),
@@ -34,22 +39,58 @@ class ExploreScreen extends StatelessWidget {
   }
 }
 
-class ListWidget extends StatelessWidget {
+class ListWidget extends StatefulWidget {
   final List<NftModel> items;
+  final int duration;
   const ListWidget({
     super.key,
     required this.items,
+    this.duration = 100,
   });
+
+  @override
+  State<ListWidget> createState() => _ListWidgetState();
+}
+
+class _ListWidgetState extends State<ListWidget> {
+  late ScrollController _scrollController;
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.position.atEdge) {
+        _autoScroll();
+      }
+    });
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _autoScroll();
+    });
+  }
+
+  _autoScroll() {
+    final currentScrollPosition = _scrollController.offset;
+    final scrollEndPosition = _scrollController.position.maxScrollExtent;
+    scheduleMicrotask(() {
+      _scrollController.animateTo(
+          currentScrollPosition == scrollEndPosition ? 0 : scrollEndPosition,
+          duration: Duration(seconds: widget.duration),
+          curve: Curves.linear);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 165,
       child: ListView.builder(
-          itemCount: items.length,
+          reverse: true,
+         controller: _scrollController,
+          itemCount: 50,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index) {
-            final item = items[index];
+            int imageIndex = index % widget.items.length;
+            final item = widget.items[imageIndex];
             return Padding(
               padding: const EdgeInsets.all(6),
               child: Container(
@@ -63,5 +104,11 @@ class ListWidget extends StatelessWidget {
             );
           }),
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
