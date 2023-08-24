29c9dfe9daa69cfa8c3f7b3d402553eb338b87fd
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-08-24 23:02:18 +0330

    add fluttergen package

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,256 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/widgets.dart';
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/arrow_icon.svg
+  String get arrowIcon => 'assets/icons/arrow_icon.svg';
+
+  /// File path: assets/icons/back_icon.svg
+  String get backIcon => 'assets/icons/back_icon.svg';
+
+  /// File path: assets/icons/cart_icon.svg
+  String get cartIcon => 'assets/icons/cart_icon.svg';
+
+  /// File path: assets/icons/ethereum_icon.svg
+  String get ethereumIcon => 'assets/icons/ethereum_icon.svg';
+
+  /// File path: assets/icons/filter_icon.svg
+  String get filterIcon => 'assets/icons/filter_icon.svg';
+
+  /// File path: assets/icons/menu_icon.svg
+  String get menuIcon => 'assets/icons/menu_icon.svg';
+
+  /// File path: assets/icons/placebid_icon.svg
+  String get placebidIcon => 'assets/icons/placebid_icon.svg';
+
+  /// File path: assets/icons/tick_icon.svg
+  String get tickIcon => 'assets/icons/tick_icon.svg';
+
+  /// File path: assets/icons/wallet_icon.svg
+  String get walletIcon => 'assets/icons/wallet_icon.svg';
+
+  /// List of all assets
+  List<String> get values => [
+        arrowIcon,
+        backIcon,
+        cartIcon,
+        ethereumIcon,
+        filterIcon,
+        menuIcon,
+        placebidIcon,
+        tickIcon,
+        walletIcon
+      ];
+}
+
+class $AssetsImagesGen {
+  const $AssetsImagesGen();
+
+  /// File path: assets/images/home_header_image.png
+  AssetGenImage get homeHeaderImage =>
+      const AssetGenImage('assets/images/home_header_image.png');
+
+  /// File path: assets/images/home_item_1.png
+  AssetGenImage get homeItem1 =>
+      const AssetGenImage('assets/images/home_item_1.png');
+
+  /// File path: assets/images/home_item_2.png
+  AssetGenImage get homeItem2 =>
+      const AssetGenImage('assets/images/home_item_2.png');
+
+  /// File path: assets/images/home_item_3.png
+  AssetGenImage get homeItem3 =>
+      const AssetGenImage('assets/images/home_item_3.png');
+
+  /// File path: assets/images/home_item_4.png
+  AssetGenImage get homeItem4 =>
+      const AssetGenImage('assets/images/home_item_4.png');
+
+  /// File path: assets/images/home_item_5.png
+  AssetGenImage get homeItem5 =>
+      const AssetGenImage('assets/images/home_item_5.png');
+
+  /// File path: assets/images/home_item_6.png
+  AssetGenImage get homeItem6 =>
+      const AssetGenImage('assets/images/home_item_6.png');
+
+  /// File path: assets/images/image_avatar.png
+  AssetGenImage get imageAvatar =>
+      const AssetGenImage('assets/images/image_avatar.png');
+
+  /// File path: assets/images/nft_1_1.png
+  AssetGenImage get nft11 => const AssetGenImage('assets/images/nft_1_1.png');
+
+  /// File path: assets/images/nft_1_2.png
+  AssetGenImage get nft12 => const AssetGenImage('assets/images/nft_1_2.png');
+
+  /// File path: assets/images/nft_1_3.png
+  AssetGenImage get nft13 => const AssetGenImage('assets/images/nft_1_3.png');
+
+  /// File path: assets/images/nft_1_4.png
+  AssetGenImage get nft14 => const AssetGenImage('assets/images/nft_1_4.png');
+
+  /// File path: assets/images/nft_2_1.png
+  AssetGenImage get nft21 => const AssetGenImage('assets/images/nft_2_1.png');
+
+  /// File path: assets/images/nft_2_2.png
+  AssetGenImage get nft22 => const AssetGenImage('assets/images/nft_2_2.png');
+
+  /// File path: assets/images/nft_2_3.png
+  AssetGenImage get nft23 => const AssetGenImage('assets/images/nft_2_3.png');
+
+  /// File path: assets/images/nft_2_4.png
+  AssetGenImage get nft24 => const AssetGenImage('assets/images/nft_2_4.png');
+
+  /// File path: assets/images/nft_3_1.png
+  AssetGenImage get nft31 => const AssetGenImage('assets/images/nft_3_1.png');
+
+  /// File path: assets/images/nft_3_2.png
+  AssetGenImage get nft32 => const AssetGenImage('assets/images/nft_3_2.png');
+
+  /// File path: assets/images/nft_3_3.png
+  AssetGenImage get nft33 => const AssetGenImage('assets/images/nft_3_3.png');
+
+  /// File path: assets/images/nft_3_4.png
+  AssetGenImage get nft34 => const AssetGenImage('assets/images/nft_3_4.png');
+
+  /// File path: assets/images/nft_3_5.png
+  AssetGenImage get nft35 => const AssetGenImage('assets/images/nft_3_5.png');
+
+  /// File path: assets/images/nft_4_1.png
+  AssetGenImage get nft41 => const AssetGenImage('assets/images/nft_4_1.png');
+
+  /// File path: assets/images/nft_4_2.png
+  AssetGenImage get nft42 => const AssetGenImage('assets/images/nft_4_2.png');
+
+  /// File path: assets/images/nft_4_3.png
+  AssetGenImage get nft43 => const AssetGenImage('assets/images/nft_4_3.png');
+
+  /// File path: assets/images/nft_4_4.png
+  AssetGenImage get nft44 => const AssetGenImage('assets/images/nft_4_4.png');
+
+  /// File path: assets/images/owner_picture.png
+  AssetGenImage get ownerPicture =>
+      const AssetGenImage('assets/images/owner_picture.png');
+
+  /// List of all assets
+  List<AssetGenImage> get values => [
+        homeHeaderImage,
+        homeItem1,
+        homeItem2,
+        homeItem3,
+        homeItem4,
+        homeItem5,
+        homeItem6,
+        imageAvatar,
+        nft11,
+        nft12,
+        nft13,
+        nft14,
+        nft21,
+        nft22,
+        nft23,
+        nft24,
+        nft31,
+        nft32,
+        nft33,
+        nft34,
+        nft35,
+        nft41,
+        nft42,
+        nft43,
+        nft44,
+        ownerPicture
+      ];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+  static const $AssetsImagesGen images = $AssetsImagesGen();
+}
+
+class AssetGenImage {
+  const AssetGenImage(this._assetName);
+
+  final String _assetName;
+
+  Image image({
+    Key? key,
+    AssetBundle? bundle,
+    ImageFrameBuilder? frameBuilder,
+    ImageErrorWidgetBuilder? errorBuilder,
+    String? semanticLabel,
+    bool excludeFromSemantics = false,
+    double? scale,
+    double? width,
+    double? height,
+    Color? color,
+    Animation<double>? opacity,
+    BlendMode? colorBlendMode,
+    BoxFit? fit,
+    AlignmentGeometry alignment = Alignment.center,
+    ImageRepeat repeat = ImageRepeat.noRepeat,
+    Rect? centerSlice,
+    bool matchTextDirection = false,
+    bool gaplessPlayback = false,
+    bool isAntiAlias = false,
+    String? package,
+    FilterQuality filterQuality = FilterQuality.low,
+    int? cacheWidth,
+    int? cacheHeight,
+  }) {
+    return Image.asset(
+      _assetName,
+      key: key,
+      bundle: bundle,
+      frameBuilder: frameBuilder,
+      errorBuilder: errorBuilder,
+      semanticLabel: semanticLabel,
+      excludeFromSemantics: excludeFromSemantics,
+      scale: scale,
+      width: width,
+      height: height,
+      color: color,
+      opacity: opacity,
+      colorBlendMode: colorBlendMode,
+      fit: fit,
+      alignment: alignment,
+      repeat: repeat,
+      centerSlice: centerSlice,
+      matchTextDirection: matchTextDirection,
+      gaplessPlayback: gaplessPlayback,
+      isAntiAlias: isAntiAlias,
+      package: package,
+      filterQuality: filterQuality,
+      cacheWidth: cacheWidth,
+      cacheHeight: cacheHeight,
+    );
+  }
+
+  ImageProvider provider({
+    AssetBundle? bundle,
+    String? package,
+  }) {
+    return AssetImage(
+      _assetName,
+      bundle: bundle,
+      package: package,
+    );
+  }
+
+  String get path => _assetName;
+
+  String get keyName => _assetName;
+}
